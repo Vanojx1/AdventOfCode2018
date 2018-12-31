cad865c3f077d1c409a7216b9c1e6d534b5fcb2f
Vanojx1 <nicolavanoni.dev@gmail.com>
2018-12-31 16:44:13 +0100

    Done D12, D13

---
+++ D12/part2.py
@@ -0,0 +1,60 @@
+import re
+
+puzzleInput = open('input.txt', 'r').read()
+
+pots = re.match(r'initial state: ([#\.]+)', puzzleInput).group(1)
+pots = { i:v for i, v in enumerate(pots) }
+
+notes = re.findall(r'([#\.]+) => ([#\.])', puzzleInput)
+notes = { k:v for k, v in notes }
+
+def getPotState(potIndex):
+  def getChar():
+    for i in range(-2, 3, 1):
+      if potIndex+i in pots:
+        yield pots[potIndex+i]
+  return ''.join(getChar())
+
+total = 0
+gen = 1
+lastDiff = 0
+lastTotal = 0
+while 1:
+  # print 'P:', ''.join(map(lambda x: pots[x], sorted(pots.keys())))
+  minPotIndex = min(pots.keys())
+  maxPotIndex = max(pots.keys())
+  for i in range(minPotIndex-1,  minPotIndex-6, -1):
+    pots[i] = '.'
+  for i in range(maxPotIndex+1,  maxPotIndex+6):
+    pots[i] = '.'
+  nextPots = {}
+  for i in sorted(pots.keys()):
+    pState = getPotState(i)
+    if pState in notes:
+      nextPots[i]=notes[pState]
+    else:
+      nextPots[i]='.'
+  clearPots = dict(nextPots)
+  for i in sorted(nextPots.keys()):
+    if nextPots[i] == '.':
+      del clearPots[i]
+    else:
+      break
+  for i in reversed(sorted(nextPots.keys())):
+    if nextPots[i] == '.':
+      del clearPots[i]
+    else:
+      break
+  # print 'N: '+''.join(map(lambda x: clearPots[x], sorted(clearPots.keys())))
+  pots = clearPots
+  total=sum([k for k, v in pots.iteritems() if v == '#'])
+  diff = total - lastTotal
+  if lastDiff == diff:
+    result = lastTotal+(50000000000-(gen-1))*diff
+    break
+  lastDiff = total - lastTotal
+  lastTotal = total
+  gen+=1
+  # print '--------------------------------'
+
+print 'Result: %s' % result
---
+++ D13/part2.py
@@ -0,0 +1,167 @@
+import pygame
+import numpy as np
+from collections import deque
+
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+RED = (255,0,0)
+GRAY = (221,221,221)
+
+puzzleInput = open('input.txt', 'r').read().split('\n')
+mineGrid = map(lambda x: list(x), puzzleInput)
+
+def getTrackSize(x, y):
+  width = 1
+  height = 1
+  i = 0
+  while mineGrid[y][x+i] != '\\':
+    width+=1
+    i+=1
+  j=0
+  while mineGrid[y+j][x+i] != '/':
+    height+=1
+    j+=1
+  return (width, height)
+
+def drawTrack(track, screen, wratio, hratio):
+  origin = track['origin']
+  size  = track['size']
+
+  coords = [
+    [origin[0], origin[1]],
+    [origin[0]+size[0]-1, origin[1]],
+    [origin[0]+size[0]-1, origin[1]+size[1]-1],
+    [origin[0], origin[1]+size[1]-1]
+  ]
+
+  pygame.draw.polygon(
+    screen, 
+    GRAY, 
+    np.multiply(coords, (wratio, hratio)),
+    3)
+
+tracks = []
+for y, row in enumerate(mineGrid):
+  for x, col in enumerate(row):
+    cell = mineGrid[y][x]
+    if cell == '/' and \
+      x+1 < len(mineGrid[y]) and mineGrid[y][x+1] in ['+', '-', '<', '>'] and \
+      y+1 < len(mineGrid) and mineGrid[y+1][x] in ['+', '|', '^', 'v']:
+      tracks.append({
+        'origin': (x, y),
+        'size': getTrackSize(x, y)
+      })
+
+maxW = 0
+maxH = 0
+for track in tracks:
+  cw, ch = (track['origin'][0]+track['size'][0], track['origin'][1]+track['size'][1])
+  if cw > maxW:
+    maxW = cw
+  if ch > maxH:
+    maxH = ch
+
+pygame.init()
+
+screen = pygame.display.set_mode([800, 800])
+pygame.display.set_caption("AOC2018 DAY 13")
+
+carts = {}
+for y, row in enumerate(mineGrid):
+  for x, col in enumerate(row):
+    if col in ['<', '>', '^', 'v']:
+      orderedDir = list(reversed([(0, -1), (1, 0), (0, 1), (-1, 0)]))
+      if col == '<':
+        cartDir = (-1, 0)
+      elif col == '>':
+        cartDir = (1, 0)
+      elif col == '^':
+        cartDir = (0, -1)
+      elif col == 'v':
+        cartDir = (0, 1)
+      carts[(x, y)] = (deque(map(lambda x: orderedDir[orderedDir.index(cartDir)-x], range(4))), deque('LFR'))
+
+FPS=60
+done=False
+clock = pygame.time.Clock()
+result = (None, None)
+while not done:
+
+  clock.tick(FPS)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      done=True
+
+  screen.fill(WHITE)
+
+  sw, sh = pygame.display.get_surface().get_size()
+  wratio, hratio = sw/float(maxW), sh/float(maxH)
+
+  for track in tracks:
+    drawTrack(track, screen, wratio, hratio)
+
+  if len(carts.keys()) > 1:
+    for (x, y), (dirs, interDir) in sorted(carts.iteritems(), key=lambda (k, v): k):
+      
+      if (x, y) not in carts:
+        continue
+      
+      del carts[(x, y)]
+
+      dx, dy = dirs[0]
+      currCell = mineGrid[y][x]
+      if currCell == '\\':
+        if dx != 0:
+          dirs.rotate(-1)
+        else:
+          dirs.rotate(1)
+      elif currCell == '/':
+        if dx != 0:
+          dirs.rotate(1)
+        else:
+          dirs.rotate(-1)
+      elif currCell == '+':
+        nextInterDir = interDir[0]
+        if nextInterDir == 'L':
+          dirs.rotate(1)
+        elif nextInterDir == 'R':
+          dirs.rotate(-1)
+        interDir.rotate(-1)
+      dx, dy = dirs[0]
+
+      nextPos = (x+dx, y+dy)
+
+      if nextPos in carts:
+        del carts[nextPos]
+        continue
+
+      coords = [
+        nextPos[0]*wratio-1.5,
+        nextPos[1]*hratio-1.5,
+        3,
+        3
+      ]
+      pygame.draw.rect(
+        screen,
+        BLACK,
+        coords)
+    
+      carts[nextPos] = (dirs, interDir)
+
+  else:
+    result = carts.keys().pop()
+    coords = [
+      result[0]*wratio-1.5,
+      result[1]*hratio-1.5,
+      3,
+      3
+    ]
+    pygame.draw.rect(
+      screen,
+      RED,
+      coords)
+
+  pygame.display.flip()
+    
+
+print 'Result: %s,%s' % result
